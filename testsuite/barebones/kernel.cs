ba1dbb0d9b62215ea1e66b53bf18788a0b86e4d7
John Cronin <jncronin@tysos.org>
2014-08-28 15:16:47 +0000

    Added osdev BareBones tutorial to testsuite
    git-svn-id: https://www.tysos.org/svn/branches/tysila3@561 21779fe5-bacf-4c11-b3ef-82bd0e1f58e8

---
+++ testsuite/barebones/kernel.cs
@@ -0,0 +1,29 @@
+namespace BareBones
+{
+	class Program
+	{
+		static int pos = 0;
+
+		unsafe static void Main()
+		{
+			for(int i = 0; i < 80 * 25 * 2; i++)
+				*(byte *)(0xb8000 + i) = 0;
+
+			Print("Hello World!");
+		}
+
+		static void Print(string s)
+		{
+			foreach(char c in s)
+				Print(c);
+		}
+
+		unsafe static void Print(char c)
+		{
+			*(byte *)(0xb8000 + pos) = (byte)c;
+			*(byte *)(0xb8000 + pos + 1) = 0x0f;
+			pos += 2;
+		}
+	}
+}
+
