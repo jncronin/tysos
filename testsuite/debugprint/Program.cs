10640d70eedb4d54317b8e5a66a3d41c2bac48ad
John Cronin <jncronin@tysos.org>
2015-07-22 19:23:16 +0000

    debugprint: Added test process
    git-svn-id: https://www.tysos.org/svn/branches/tysila3@614 21779fe5-bacf-4c11-b3ef-82bd0e1f58e8

---
+++ testsuite/debugprint/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace debugprint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Debugger.Log(0, "debugprint", "Waiting for logger to be available...");
+            tysos.ServerObject logger = null;
+
+            while (logger == null)
+                logger = tysos.Syscalls.ProcessFunctions.GetSpecialProcess(tysos.Syscalls.ProcessFunctions.SpecialProcessType.Logger);
+
+            System.Diagnostics.Debugger.Log(0, "debugprint", " Logger available\n");
+
+            while (true)
+            {
+                logger.Invoke("LogMessage", new object[] { "debugprint", 0, "Remote log message\n" });
+            }
+        }
+    }
+}
