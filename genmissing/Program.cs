28a44fe09b723c7d2ccd5e53f646900f6980be1e
John Cronin <jncronin@tysos.org>
2018-01-05 21:04:47 +0000

    genmissing: add code to actually create the stub functions

--- genmissing/Program.cs
+++ genmissing/Program.cs
@@ -19,9 +19,10 @@
  * THE SOFTWARE.
  */
 
- using System;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using binary_library.elf;
 
 namespace genmissing
 {
@@ -38,6 +39,14 @@ namespace genmissing
             @"./tysos/x86_64/switcher.o",
             @"D:\tysos\metadata\bin\Release/metadata.obj",
         };
+
+        static string[] lib_paths = new string[]
+        {
+
+        };
+
+        static string arch = "x86_64";
+        static string output_name = "output.o";        
 
         static void Main(string[] args)
         {
@@ -65,6 +74,102 @@ namespace genmissing
                 ifobj.Read();
                 ifiles.Add(ifobj);
             }
+
+            /* Get a list of all defined symbols */
+            var def_syms = new Dictionary<string, binary_library.ISymbol>();
+            foreach(var file in ifiles)
+            {
+                var sym_count = file.GetSymbolCount();
+                for(int i = 0; i < sym_count; i++)
+                {
+                    var sym = file.GetSymbol(i);
+                    if (sym.DefinedIn != null)
+                        def_syms[sym.Name] = sym;
+                }
+            }
+
+            /* Get a list of those relocations which are missing */
+            var missing = new Dictionary<string, binary_library.ISymbol>();
+            foreach(var file in ifiles)
+            {
+                var reloc_count = file.GetRelocationCount();
+                for(int i = 0; i < reloc_count; i++)
+                {
+                    var reloc = file.GetRelocation(i);
+                    if (reloc.References != null && !def_syms.ContainsKey(reloc.References.Name))
+                        missing[reloc.References.Name] = reloc.References;
+                }
+            }
+
+            /* Generate an output object */
+            var o = new binary_library.elf.ElfFile(ifiles[0].Bitness);
+            o.Init();
+
+            o.Filename = output_name;
+
+            tysila4.Program.search_dirs.Add(@"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.6.1");
+            tysila4.Program.search_dirs.Add(@"D:\tysos\tysos\bin\Release");
+
+            /* Generate dummy module */
+            var ms = new metadata.MetadataStream();
+            ms.al = new libtysila5.libtysila.AssemblyLoader(new tysila4.FileSystemFileLoader());
+            ms.LoadBuiltinTypes();
+
+            /* Load up assembler target */
+            var t = libtysila5.target.Target.targets[arch];
+            var fsi = new System.IO.FileInfo(output_name);
+            t.st = new libtysila5.StringTable(fsi.Name.Substring(0, fsi.Name.Length - fsi.Extension.Length), ms.al, t);
+
+            /* Generate signature of missing_function(string) to call */
+            var special_c = new libtysila5.Code { ms = new metadata.MethodSpec { m = ms } };
+            var special = special_c.special_meths;
+            var missing_msig = special.CreateMethodSignature(null, new metadata.TypeSpec[] { ms.SystemString });
+
+            /* Generate stub functions for missing methods */
+            var m = missing.Keys;
+            var missing_types = new List<string>();
+            var other_missing = new List<string>();
+            foreach(var str in m)
+            {
+                try
+                {
+                    if (ms.IsMangledMethod(str))
+                    {
+                        GenerateMissingMethod(str, ms, t, missing_msig, o);
+                    }
+                    else
+                        missing_types.Add(str);
+                }
+                catch(ArgumentException)
+                {
+                    other_missing.Add(str);
+                }
+            }
+        }
+
+        private static void GenerateMissingMethod(string str, metadata.MetadataStream m, libtysila5.target.Target t, int missing_msig, ElfFile o)
+        {
+            /* Generate new method spec */
+            var ms = new metadata.MethodSpec { m = m, mangle_override = str };
+
+            /* Generate stub code */
+            libtysila5.Code c = new libtysila5.Code();
+            c.t = t;
+            c.ms = ms;
+
+            var ir = new List<libtysila5.cil.CilNode.IRNode>();
+            var stack = new libtysila5.util.Stack<libtysila5.ir.StackItem>();
+            var cilnode = new libtysila5.cil.CilNode(ms, 0);
+            cilnode.irnodes = ir;
+            c.ir = ir;
+            c.starts = new List<libtysila5.cil.CilNode> { cilnode };
+
+            stack = libtysila5.ir.ConvertToIR.ldstr(cilnode, c, stack, str);
+            stack = libtysila5.ir.ConvertToIR.call(cilnode, c, stack, false, "missing_function",
+                c.special_meths, missing_msig);
+
+            /* Assemble */
+            libtysila5.libtysila.AssembleMethod(ms, o, t, null, null, c);
         }
     }
 }
